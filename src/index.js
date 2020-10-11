b8e950bdc39d9c946dd84874066d5dce5d20d8c5
Utku Gultopu <ugultopu@gmail.com>
2020-10-11 16:37:27 -0400

    Add react-apollo dependency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
@@ -26,6 +27,8 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider {...{client}}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
